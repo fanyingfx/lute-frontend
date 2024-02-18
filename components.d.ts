5e2c1dbb6bc8d5bd6cef123f95a9ee01529e73c2
fanyingfx <fanyingfx@outlook.com>
2024-02-18 09:22:10 +0800

    switch to windows

--- components.d.ts
+++ components.d.ts
@@ -7,7 +7,6 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
     IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
@@ -27,20 +26,13 @@ declare module 'vue' {
     NP: typeof import('naive-ui')['NP']
     NPopover: typeof import('naive-ui')['NPopover']
     NSlider: typeof import('naive-ui')['NSlider']
-    NSpace: typeof import('naive-ui')['NSpace']
     NSplit: typeof import('naive-ui')['NSplit']
     NText: typeof import('naive-ui')['NText']
-    Reading: typeof import('./src/components/reading/Reading.vue')['default']
     ReadingPage: typeof import('./src/components/reading/ReadingPage.vue')['default']
-    ReadingView: typeof import('./src/views/ReadingView.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    Sentence: typeof import('./src/components/reading/Sentence.vue')['default']
     SentenceItem: typeof import('./src/components/reading/SentenceItem.vue')['default']
     TextParagraph: typeof import('./src/components/reading/TextParagraph.vue')['default']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
-    Word: typeof import('./src/components/reading/Word.vue')['default']
     WordForm: typeof import('./src/components/reading/WordForm.vue')['default']
     WordItem: typeof import('./src/components/reading/WordItem.vue')['default']
     WordText: typeof import('./src/components/reading/WordText.vue')['default']
